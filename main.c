b14e9f52263b5623f624fe20f2d2fcab56cae788
Mohammad Momeni <moehmeni@gmail.com>
2024-03-16 15:43:37 +0100

    Initial parsing process

--- main.c
+++ main.c
@@ -6,16 +6,25 @@
 /*   By: mmomeni <mmomeni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 19:25:50 by mmomeni           #+#    #+#             */
-/*   Updated: 2024/03/04 19:30:49 by mmomeni          ###   ########.fr       */
+/*   Updated: 2024/03/09 18:18:56 by mmomeni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./miniRT.h"
+
+
 int main(int argc, char **argv)
 {
+    int fd;
+    
     if (argc != 2)
+        return (ft_putstr("Usage: ./miniRT [file.rt]\n"), 1);
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
+        return (ft_putstr("Error\n"), 1);
+    while (get_next_line((fd)))
     {
-        ft_putstr("Usage: ./miniRT [file.rt]\n");
-        return (1);
+        
     }
     return (0);
 }
